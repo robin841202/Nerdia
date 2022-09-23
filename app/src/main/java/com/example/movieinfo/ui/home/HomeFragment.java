546605f5189970e68c2823301fbb1386cde7b40a
robinhsueh <robin841202@gmail.com>
2022-09-23 19:24:04 +0800

    - feat: fetching upcoming movies data - feat: fetching now-playing movies data

--- app/src/main/java/com/example/movieinfo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/movieinfo/ui/home/HomeFragment.java
@@ -1,66 +1,227 @@
 package com.example.movieinfo.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.movieinfo.R;
+import com.example.movieinfo.model.movie.MovieData;
+import com.example.movieinfo.model.repository.MovieRepository;
+import com.example.movieinfo.view.MoviesAdapter;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private final String LOG_TAG = "HomeFragment";
+    private final MovieRepository movieRepository = new MovieRepository();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private SwipeRefreshLayout pullToRefresh;
+
+    private ProgressBar upcomingMovies_PB;
+    private MoviesAdapter upcomingMoviesAdapter;
+    private RecyclerView upcomingMovies_RcView;
+    private LinearLayoutManager upcomingMoviesLayoutMgr;
+    private int upcomingMoviesPage;
+
+    private ProgressBar nowPlayingMovies_PB;
+    private MoviesAdapter nowPlayingMoviesAdapter;
+    private RecyclerView nowPlayingMovies_RcView;
+    private LinearLayoutManager nowPlayingMoviesLayoutMgr;
+    private int nowPlayingMoviesPage;
+
+
+
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // set default page
+        upcomingMoviesPage = 1;
+        nowPlayingMoviesPage = 1;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Get Views
+        upcomingMovies_PB = root.findViewById(R.id.pb_upcoming_movies);
+        upcomingMovies_RcView = root.findViewById(R.id.recycler_upcoming_movies);
+        nowPlayingMovies_PB = root.findViewById(R.id.pb_now_playing_movies);
+        nowPlayingMovies_RcView = root.findViewById(R.id.recycler_now_playing_movies);
+        pullToRefresh = root.findViewById(R.id.swiperefresh);
+
+        // Initialize movieAdapter
+        upcomingMoviesAdapter = new MoviesAdapter();
+        nowPlayingMoviesAdapter = new MoviesAdapter();
+        // Initialize linearLayoutManager
+        upcomingMoviesLayoutMgr = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        nowPlayingMoviesLayoutMgr = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        // Set adapter
+        upcomingMovies_RcView.setAdapter(upcomingMoviesAdapter);
+        nowPlayingMovies_RcView.setAdapter(nowPlayingMoviesAdapter);
+        // Set layoutManager
+        upcomingMovies_RcView.setLayoutManager(upcomingMoviesLayoutMgr);
+        nowPlayingMovies_RcView.setLayoutManager(nowPlayingMoviesLayoutMgr);
+
+        // Start getting data
+        getUpcomingMovies();
+        getNowPlayingMovies();
+
+        // Set SwipeRefreshListener
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getUpcomingMovies();
+                Log.d(LOG_TAG, "onRefreshUpcoming");
+
+                getNowPlayingMovies();
+                Log.d(LOG_TAG, "onRefreshNowPlaying");
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
+        return root;
     }
+
+
+    /**
+     * Get Upcoming Movies
+     */
+    public void getUpcomingMovies() {
+        try {
+            // show progressBar
+            upcomingMovies_PB.setVisibility(View.VISIBLE);
+            Method onUpcomingMoviesFetched = getClass().getMethod("onUpcomingMoviesFetched", ArrayList.class);
+            Method onFetchDataError = getClass().getMethod("onFetchDataError");
+            movieRepository.getUpcomingMovies(upcomingMoviesPage, this, onUpcomingMoviesFetched, onFetchDataError);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Callback when get upcoming movies successfully
+     * @param movie_list upcoming movies data
+     */
+    public void onUpcomingMoviesFetched(ArrayList<MovieData> movie_list) {
+        // hide progressBar
+        upcomingMovies_PB.setVisibility(View.GONE);
+
+        // append data to adapter
+        upcomingMoviesAdapter.appendMovies(movie_list);
+
+        // attach onScrollListener to RecyclerView
+        upcomingMovies_RcView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                // get the number of all items in recyclerView
+                int totalItemCount = upcomingMoviesLayoutMgr.getItemCount();
+                // get the number of current items attached to recyclerView
+                int visibleItemCount = upcomingMoviesLayoutMgr.getChildCount();
+                // get the first visible item's position
+                int firstVisibleItem = upcomingMoviesLayoutMgr.findFirstVisibleItemPosition();
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2){
+                    // detach current OnScrollListener
+                    upcomingMovies_RcView.removeOnScrollListener(this);
+
+                    // append nextPage data to recyclerView
+                    upcomingMoviesPage++;
+                    getUpcomingMovies();
+                }
+            }
+        });
+
+        Log.d(LOG_TAG, "upcoming movies: data fetched successfully");
+    }
+
+
+
+    /**
+     * Get Now-Playing Movies
+     */
+    public void getNowPlayingMovies() {
+        try {
+            // show progressBar
+            nowPlayingMovies_PB.setVisibility(View.VISIBLE);
+            Method onNowPlayingMoviesFetched = getClass().getMethod("onNowPlayingMoviesFetched", ArrayList.class);
+            Method onFetchDataError = getClass().getMethod("onFetchDataError");
+            movieRepository.getNowPlayingMovies(nowPlayingMoviesPage, this, onNowPlayingMoviesFetched, onFetchDataError);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Callback when get now-playing movies successfully
+     * @param movie_list now-playing movies data
+     */
+    public void onNowPlayingMoviesFetched(ArrayList<MovieData> movie_list) {
+        // hide progressBar
+        nowPlayingMovies_PB.setVisibility(View.GONE);
+        // append data to adapter
+        nowPlayingMoviesAdapter.appendMovies(movie_list);
+        // attach onScrollListener to RecyclerView
+        nowPlayingMovies_RcView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                // get the number of all items in recyclerView
+                int totalItemCount = nowPlayingMoviesLayoutMgr.getItemCount();
+                // get the number of current items attached to recyclerView
+                int visibleItemCount = nowPlayingMoviesLayoutMgr.getChildCount();
+                // get the first visible item's position
+                int firstVisibleItem = nowPlayingMoviesLayoutMgr.findFirstVisibleItemPosition();
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2){
+                    // detach current OnScrollListener
+                    nowPlayingMovies_RcView.removeOnScrollListener(this);
+
+                    // append nextPage data to recyclerView
+                    nowPlayingMoviesPage++;
+                    getNowPlayingMovies();
+                }
+            }
+        });
+
+        Log.d(LOG_TAG, "nowPlaying movies: data fetched successfully");
+    }
+
+
+
+
+    /**
+     * Callback when data fetched fail
+     */
+    public void onFetchDataError() {
+        Log.d(LOG_TAG, "data fetched fail");
+    }
+
+
+
+
+
 }
