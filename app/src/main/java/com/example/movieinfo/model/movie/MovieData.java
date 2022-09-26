728568a3b036039e628127ef071b9cfea682967c
robinhsueh <robin841202@gmail.com>
2022-09-26 18:35:51 +0800

    首頁 > 新增本週熱門電影瀏覽功能  - add TrendingMovie
    實作Parcelable達到可於各Activity間傳遞object資料
     - implement Parcelable on MovieData in order to pass data object to other Activities

--- app/src/main/java/com/example/movieinfo/model/movie/MovieData.java
+++ app/src/main/java/com/example/movieinfo/model/movie/MovieData.java
@@ -1,11 +1,14 @@
 package com.example.movieinfo.model.movie;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Movie Data Model, using @SerializedName to map to json key
  */
-public class MovieData {
+public class MovieData implements Parcelable {
 
     @SerializedName("id")
     private long id;
@@ -28,7 +31,7 @@ public class MovieData {
     @SerializedName("release_date")
     private String releaseDate;
 
-    public MovieData(long id, String title, String overview, String posterPath, String backdropPath, double rating, String releaseDate){
+    public MovieData(long id, String title, String overview, String posterPath, String backdropPath, double rating, String releaseDate) {
         this.id = id;
         this.title = title;
         this.overview = overview;
@@ -42,6 +45,7 @@ public class MovieData {
 
     /**
      * Get Movie Id
+     *
      * @return Movie Id
      */
     public long getId() {
@@ -50,6 +54,7 @@ public class MovieData {
 
     /**
      * Get Movie Title
+     *
      * @return Movie Title
      */
     public String getTitle() {
@@ -58,6 +63,7 @@ public class MovieData {
 
     /**
      * Get Movie Overview
+     *
      * @return Movie Overview
      */
     public String getOverview() {
@@ -66,6 +72,7 @@ public class MovieData {
 
     /**
      * Get Movie Poster Url
+     *
      * @return Movie Poster Url
      */
     public String getPosterPath() {
@@ -74,6 +81,7 @@ public class MovieData {
 
     /**
      * Get Movie Backdrop Url
+     *
      * @return Movie Backdrop Url
      */
     public String getBackdropPath() {
@@ -82,6 +90,7 @@ public class MovieData {
 
     /**
      * Get Movie Rating
+     *
      * @return Movie Rating
      */
     public double getRating() {
@@ -90,6 +99,7 @@ public class MovieData {
 
     /**
      * Set Movie Rating
+     *
      * @param rating Movie Rating
      */
     public void setRating(double rating) {
@@ -98,9 +108,71 @@ public class MovieData {
 
     /**
      * Get Movie Release Date
+     *
      * @return Release Date
      */
     public String getReleaseDate() {
         return releaseDate;
+    }
+
+
+
+
+
+    /**
+     * 新增的Parcelable部分，讀取參數，參數順序要和建構子一樣
+     * @param in
+     */
+    protected MovieData(Parcel in) {
+        id = in.readLong();
+        title = in.readString();
+        overview = in.readString();
+        posterPath = in.readString();
+        backdropPath = in.readString();
+        rating = in.readDouble();
+        releaseDate = in.readString();
+    }
+
+
+    /**
+     * 新增的Parcelable部分
+     */
+    public static final Creator<MovieData> CREATOR = new Creator<MovieData>() {
+        @Override
+        public MovieData createFromParcel(Parcel in) {
+            return new MovieData(in);
+        }
+
+        @Override
+        public MovieData[] newArray(int size) {
+            return new MovieData[size];
+        }
+    };
+
+
+    /**
+     * 新增的Parcelable部分，Describe the kinds of special objects contained in this Parcelable instance's marshaled representation.
+     * @return
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
+    /**
+     * 新增的Parcelable部分，將物件攤平成Parcel，參數順序要和建構子一樣
+     * @param dest
+     * @param flags
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(title);
+        dest.writeString(overview);
+        dest.writeString(posterPath);
+        dest.writeString(backdropPath);
+        dest.writeDouble(rating);
+        dest.writeString(releaseDate);
     }
 }
--- app/src/main/java/com/example/movieinfo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/movieinfo/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.movieinfo.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,8 +17,10 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.example.movieinfo.R;
+import com.example.movieinfo.model.StaticParameter;
 import com.example.movieinfo.model.movie.MovieData;
 import com.example.movieinfo.model.repository.MovieRepository;
+import com.example.movieinfo.view.MediaDetailsActivity;
 import com.example.movieinfo.view.MoviesAdapter;
 
 import java.lang.reflect.Method;
@@ -26,9 +29,12 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements MoviesAdapter.IMovieListener {
 
     private final String LOG_TAG = "HomeFragment";
+    // Define extra data key for passing data to other activities
+    public static final String EXTRA_DATA_MOVIE_KEY = "EXTRA_DATA_MOVIE";
+
     private final MovieRepository movieRepository = new MovieRepository();
 
     private SwipeRefreshLayout pullToRefresh;
@@ -45,7 +51,11 @@ public class HomeFragment extends Fragment {
     private LinearLayoutManager nowPlayingMoviesLayoutMgr;
     private int nowPlayingMoviesPage;
 
-
+    private ProgressBar trendingMovies_PB;
+    private MoviesAdapter trendingMoviesAdapter;
+    private RecyclerView trendingMovies_RcView;
+    private LinearLayoutManager trendingMoviesLayoutMgr;
+    private int trendingMoviesPage;
 
 
     public HomeFragment() {
@@ -58,6 +68,7 @@ public class HomeFragment extends Fragment {
         // set default page
         upcomingMoviesPage = 1;
         nowPlayingMoviesPage = 1;
+        trendingMoviesPage = 1;
     }
 
     @Override
@@ -72,34 +83,48 @@ public class HomeFragment extends Fragment {
         upcomingMovies_RcView = root.findViewById(R.id.recycler_upcoming_movies);
         nowPlayingMovies_PB = root.findViewById(R.id.pb_now_playing_movies);
         nowPlayingMovies_RcView = root.findViewById(R.id.recycler_now_playing_movies);
+        trendingMovies_PB = root.findViewById(R.id.pb_trending_movies);
+        trendingMovies_RcView = root.findViewById(R.id.recycler_trending_movies);
         pullToRefresh = root.findViewById(R.id.swiperefresh);
 
         // Initialize movieAdapter
-        upcomingMoviesAdapter = new MoviesAdapter();
-        nowPlayingMoviesAdapter = new MoviesAdapter();
+        upcomingMoviesAdapter = new MoviesAdapter(new ArrayList<>(), this);
+        nowPlayingMoviesAdapter = new MoviesAdapter(new ArrayList<>(), this);
+        trendingMoviesAdapter = new MoviesAdapter(new ArrayList<>(), this);
+
         // Initialize linearLayoutManager
         upcomingMoviesLayoutMgr = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         nowPlayingMoviesLayoutMgr = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        trendingMoviesLayoutMgr = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+
         // Set adapter
         upcomingMovies_RcView.setAdapter(upcomingMoviesAdapter);
         nowPlayingMovies_RcView.setAdapter(nowPlayingMoviesAdapter);
+        trendingMovies_RcView.setAdapter(trendingMoviesAdapter);
+
         // Set layoutManager
         upcomingMovies_RcView.setLayoutManager(upcomingMoviesLayoutMgr);
         nowPlayingMovies_RcView.setLayoutManager(nowPlayingMoviesLayoutMgr);
+        trendingMovies_RcView.setLayoutManager(trendingMoviesLayoutMgr);
 
         // Start getting data
         getUpcomingMovies();
         getNowPlayingMovies();
+        getTrendingMovies();
 
         // Set SwipeRefreshListener
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 getUpcomingMovies();
-                Log.d(LOG_TAG, "onRefreshUpcoming");
+                Log.d(LOG_TAG, "onRefreshUpcomingMovies");
 
                 getNowPlayingMovies();
-                Log.d(LOG_TAG, "onRefreshNowPlaying");
+                Log.d(LOG_TAG, "onRefreshNowPlayingMovies");
+
+                getTrendingMovies();
+                Log.d(LOG_TAG, "onRefreshTrendingMovies");
+
                 pullToRefresh.setRefreshing(false);
             }
         });
@@ -107,6 +132,7 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+    // region Upcoming Movies
 
     /**
      * Get Upcoming Movies
@@ -125,6 +151,7 @@ public class HomeFragment extends Fragment {
 
     /**
      * Callback when get upcoming movies successfully
+     *
      * @param movie_list upcoming movies data
      */
     public void onUpcomingMoviesFetched(ArrayList<MovieData> movie_list) {
@@ -145,7 +172,7 @@ public class HomeFragment extends Fragment {
                 // get the first visible item's position
                 int firstVisibleItem = upcomingMoviesLayoutMgr.findFirstVisibleItemPosition();
 
-                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2){
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
                     // detach current OnScrollListener
                     upcomingMovies_RcView.removeOnScrollListener(this);
 
@@ -159,7 +186,9 @@ public class HomeFragment extends Fragment {
         Log.d(LOG_TAG, "upcoming movies: data fetched successfully");
     }
 
+    // endregion
 
+    // region Now-Playing Movies
 
     /**
      * Get Now-Playing Movies
@@ -178,6 +207,7 @@ public class HomeFragment extends Fragment {
 
     /**
      * Callback when get now-playing movies successfully
+     *
      * @param movie_list now-playing movies data
      */
     public void onNowPlayingMoviesFetched(ArrayList<MovieData> movie_list) {
@@ -196,7 +226,7 @@ public class HomeFragment extends Fragment {
                 // get the first visible item's position
                 int firstVisibleItem = nowPlayingMoviesLayoutMgr.findFirstVisibleItemPosition();
 
-                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2){
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
                     // detach current OnScrollListener
                     nowPlayingMovies_RcView.removeOnScrollListener(this);
 
@@ -210,7 +240,61 @@ public class HomeFragment extends Fragment {
         Log.d(LOG_TAG, "nowPlaying movies: data fetched successfully");
     }
 
+    // endregion
 
+    // region Trending Movies
+
+    /**
+     * Get Trending Movies
+     */
+    public void getTrendingMovies() {
+        try {
+            // show progressBar
+            trendingMovies_PB.setVisibility(View.VISIBLE);
+            Method onTrendingMoviesFetched = getClass().getMethod("onTrendingMoviesFetched", ArrayList.class);
+            Method onFetchDataError = getClass().getMethod("onFetchDataError");
+            movieRepository.getTrendingMovies(StaticParameter.TimeWindow.WEEKLY, trendingMoviesPage, this, onTrendingMoviesFetched, onFetchDataError);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Callback when get trending movies successfully
+     *
+     * @param movie_list trending movies data
+     */
+    public void onTrendingMoviesFetched(ArrayList<MovieData> movie_list) {
+        // hide progressBar
+        trendingMovies_PB.setVisibility(View.GONE);
+        // append data to adapter
+        trendingMoviesAdapter.appendMovies(movie_list);
+        // attach onScrollListener to RecyclerView
+        trendingMovies_RcView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                // get the number of all items in recyclerView
+                int totalItemCount = trendingMoviesLayoutMgr.getItemCount();
+                // get the number of current items attached to recyclerView
+                int visibleItemCount = trendingMoviesLayoutMgr.getChildCount();
+                // get the first visible item's position
+                int firstVisibleItem = trendingMoviesLayoutMgr.findFirstVisibleItemPosition();
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    // detach current OnScrollListener
+                    trendingMovies_RcView.removeOnScrollListener(this);
+
+                    // append nextPage data to recyclerView
+                    trendingMoviesPage++;
+                    getTrendingMovies();
+                }
+            }
+        });
+
+        Log.d(LOG_TAG, "trending movies: data fetched successfully");
+    }
+
+    // endregion
 
 
     /**
@@ -221,7 +305,17 @@ public class HomeFragment extends Fragment {
     }
 
 
-
-
-
+    /**
+     * Callback when movie item get clicked
+     *
+     * @param movie movie data
+     */
+    @Override
+    public void onMovieClick(MovieData movie) {
+        Intent intent = new Intent(getContext(), MediaDetailsActivity.class);
+        intent.putExtra(EXTRA_DATA_MOVIE_KEY, movie);
+        startActivity(intent);
+        // set the custom transition animation
+        getActivity().overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+    }
 }
