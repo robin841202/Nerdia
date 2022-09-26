0b238515dace30a293c31f2fb64d5e4797f2f6cf
robinhsueh <robin841202@gmail.com>
2022-09-26 18:33:04 +0800

    首頁 > 新增評分分數於Item右上角  - add average rating score to each movie item top-right  - add rounded corner background shape resource file
    首頁 > 新增Item點擊監聽事件
     - add custom callback onMovieClick in MovieAdapter
     - implement custom callback on HomeFragment

--- app/src/main/java/com/example/movieinfo/view/MoviesAdapter.java
+++ app/src/main/java/com/example/movieinfo/view/MoviesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.movieinfo.view;
 
+import android.graphics.Movie;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +15,26 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.example.movieinfo.R;
 import com.example.movieinfo.model.movie.MovieData;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder> {
     private ArrayList<MovieData> movie_list;
+    private IMovieListener listener;
 
-    public MoviesAdapter() {
-        movie_list = new ArrayList<MovieData>();
+    public interface IMovieListener{
+        /**
+         * Movie item onClick Event
+         */
+        void onMovieClick(MovieData movie);
     }
+
+
+    public MoviesAdapter(ArrayList<MovieData> movie_list, IMovieListener listener) {
+        this.movie_list = movie_list;
+        this.listener = listener;
+    }
+
 
     @NonNull
     @Override
@@ -29,7 +42,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
         View itemView = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.item_movie, parent, false);
-        return new MoviesViewHolder(itemView);
+        return new MoviesViewHolder(itemView, listener);
     }
 
     @Override
@@ -63,24 +76,56 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
         private ImageView poster;
         private TextView title;
         private View itemView;
+        private TextView rating;
         private final String image_baseUrl = "https://image.tmdb.org/t/p/w342";
 
-        public MoviesViewHolder(@NonNull View itemView) {
+        private final IMovieListener listener;
+
+        public MoviesViewHolder(@NonNull View itemView, IMovieListener listener) {
             super(itemView);
             this.itemView = itemView;
             this.poster = itemView.findViewById(R.id.img_item_movie_poster);
             this.title = itemView.findViewById(R.id.text_item_movie_title);
+            this.rating = itemView.findViewById(R.id.text_item_movie_rating);
+
+            this.listener = listener;
         }
 
         public void bind(MovieData movieData) {
+            // set image poster
             Glide.with(itemView)
                     .load(image_baseUrl + movieData.getPosterPath())
                     .centerCrop()
                     .into(poster);
+
+            // set movie title
             title.setText(movieData.getTitle());
+
+            // set movie rating
+            double ratingNum = movieData.getRating();
+            if(ratingNum > 0){
+                // set rating TextView VISIBLE
+                rating.setVisibility(View.VISIBLE);
+                // set rating TextView
+                rating.setText(String.format("%.1f", ratingNum));
+            }else{
+                // set rating TextView GONE
+                rating.setVisibility(View.GONE);
+            }
+
+            // set item onClickListener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // callback onMovieClick method and pass movieData
+                    listener.onMovieClick(movieData);
+                }
+            });
 
         }
 
     }
+
+
 
 }
