f752b92d424d2a9c32a7eabf008345db96e7db17
robinhsueh <robin841202@gmail.com>
2022-09-30 15:38:24 +0800

    詳細資料頁面 > 背景圖放大功能  - feat: Backdrop image display in fullscreen  - feat: Zoom in & out using gesture
    詳細資料頁面 > 海報圖放大功能
     - feat: Poster image display in fullscreen
     - feat: Zoom in & out using gesture

---
+++ app/src/main/java/com/example/movieinfo/view/ImageDisplayActivity.java
@@ -0,0 +1,71 @@
+package com.example.movieinfo.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.example.movieinfo.R;
+import com.example.movieinfo.model.StaticParameter;
+
+public class ImageDisplayActivity extends AppCompatActivity {
+
+    private ImageView displayImg;
+    private ScaleGestureDetector scaleGestureDetector;
+
+    // Define image scale factor
+    private float scaleFactor = 1.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+
+        displayImg = findViewById(R.id.image_display);
+
+        // Initialize scale gesture detector for zoom in and out for image
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+        // get imgUrl from intent
+        String imgFilePath = getIntent().getStringExtra(MediaDetailsActivity.EXTRA_DATA_IMAGE_PATH_KEY);
+        if (imgFilePath != null && !imgFilePath.isEmpty()) {
+            String imgUrl = StaticParameter.getImageUrl(StaticParameter.BackdropSize.ORIGINAL, imgFilePath);
+            Glide.with(this)
+                    .load(imgUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_image_not_found)
+                    .fitCenter()
+                    .into(displayImg);
+        } else {
+            finish();
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        // Let the ScaleGestureDetector inspect all events.
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            // setting scale for image
+            scaleFactor *= scaleGestureDetector.getScaleFactor();
+            float minScaleFactor = 1.0f;
+            float maxScaleFactor = 10.0f;
+            scaleFactor = Math.max(minScaleFactor, Math.min(scaleFactor, maxScaleFactor));
+
+            // set scale x and scale y to image view.
+            displayImg.setScaleX(scaleFactor);
+            displayImg.setScaleY(scaleFactor);
+            return true;
+        }
+    }
+}
